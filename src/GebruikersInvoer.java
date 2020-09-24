0a94989885580ce58f518f7f9a859dc3f058bdf9
BKranenbarg <tenazb@gmail.com>
2020-09-24 13:34:48 +0200

    versie 2 met invoer getal en uitvoer tafel, met beter commentaar

--- src/GebruikersInvoer.java
+++ src/GebruikersInvoer.java
@@ -11,6 +11,7 @@ public class GebruikersInvoer {
 
         //string voor weergeven ingevoerde getal (nog zonder uitkomst)
         String zin = "De tafel van " + getal + " is ";
+        //zin weergeven
         System.out.println(zin);
 
         //variabele aanmaken voor output tafel
@@ -18,9 +19,13 @@ public class GebruikersInvoer {
 
         // getallen van een tot tien in lus aanmaken
         for (int x=1; x<=10; x++) {
+            //string weergeven voor tafel
             output = x + " x " + getal + " = ";
+            //variabele z waarin de cijfers uit de lus worden vermenigvuldigd met het ingevoerde getal
             int z = x * getal;
+            // de eerdere string met output van de tafel plakken aan de variable met de uitkomst
             output = output + z;
+            //output weergeven
             System.out.println(output);
         }
 
